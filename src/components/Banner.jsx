59cd43f5db16eb770e48ad16f36a8d0571baada0
sachin supunthaka <sachinsupunthaka96@gmail.com>
2022-03-16 13:05:34 +0530

    update:banner content

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
-import { Box, Button, Container, Grid, Heading, Image } from '@chakra-ui/react';
+import {
+  Badge,
+  Box,
+  Button,
+  Container,
+  Grid,
+  Heading,
+  Image,
+} from '@chakra-ui/react';
 import brandLogo from '../assets/brand-logo.svg';
 import illustration from '../assets/illustration.svg';
 import imageone from '../assets/image-one.jpg';
@@ -84,6 +92,84 @@ function Banner() {
               Exolore the outdoors
               <Image w="100%" ml="2" src={rightArrow} alt="rightArrow" />
             </Button>
+          </Box>
+        </Grid>
+      </Container>
+
+      {/* Bills */}
+      <Container maxW="container.xl">
+        <Box
+          display="flex"
+          flexDirection="row"
+          flexWrap="wrap"
+          justifyContent="space-between"
+        >
+          {/* add list of badges */}
+          <Badge
+            borderRadius="3xl"
+            px={4}
+            py={2}
+            mx="4"
+            mb="4"
+            textTransform="initial"
+            color="#ffffff"
+            bg="blue.100"
+          >
+            Boost your career
+          </Badge>
+          <Badge
+            borderRadius="3xl"
+            px={4}
+            py={2}
+            mx="4"
+            mb="4"
+            textTransform="initial"
+            color="#ffffff"
+            bg="blue.100"
+          >
+            Boost your career
+          </Badge>
+          <Badge
+            borderRadius="3xl"
+            px={4}
+            py={2}
+            mx="4"
+            mb="4"
+            textTransform="initial"
+            color="#ffffff"
+            bg="blue.100"
+          >
+            Boost your career
+          </Badge>
+          <Badge
+            borderRadius="3xl"
+            px={4}
+            py={2}
+            mx="4"
+            mb="4"
+            textTransform="initial"
+            color="#ffffff"
+            bg="blue.100"
+          >
+            Boost your career
+          </Badge>
+        </Box>
+      </Container>
+      {/* last header section */}
+      <Container maxW="container.xl" mt="20">
+        <Grid templateColumns="repeat(2, 1fr)" gap={6}>
+          <Box>
+            <Heading as="h3" size="lg" mb="7"></Heading>
+            <Box
+              display="flex"
+              alignItems="center"
+              flexDirection="row"
+              w="100%"
+            >
+              <Box flexBasis="50%" w="100%" mr="2">
+                
+              </Box>
+            </Box>
           </Box>
         </Grid>
       </Container>
